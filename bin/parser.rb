e38c31e242358c6c7df1c3b3d349a7733a025f9e
Mahmood Ahmad <mahmood073@gmail.com>
2022-07-08 14:31:36 +0200

    rubocop instructions most of them have been applied

--- bin/parser.rb
+++ bin/parser.rb
@@ -8,12 +8,12 @@ options = {}
 OptionParser.new do |parser|
   parser.banner = 'Usage: parser.rb [options]'
 
-  parser.on('--views=TYPE','Choose the views type: [ total | unique ], default is total') do |v|
+  parser.on('--views=TYPE', 'Choose the views type: [ total | unique ], default is total') do |v|
     options[:views] = v
   end
 end.parse!
 
 options[:views] ||= 'total'
 
-raise 'views should be total or unique' unless ['total', 'unique'].include? options[:views]
+raise 'views should be total or unique' unless %w(total unique).include? options[:views]
 raise 'Views log file is required' unless ARGV[0]
