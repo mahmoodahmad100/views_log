ddd154235c4977857da0d3b3f70f9bb66675db66
Mahmood Ahmad <mahmood073@gmail.com>
2022-07-09 22:33:11 +0200

    adding duplicate ip to the total test

--- spec/unit/counter/total_spec.rb
+++ spec/unit/counter/total_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Counter::Total do
 
     let(:lines) {
         [
+            "/home 184.123.665.067\n",
             "/help_page/1 126.318.035.038\n",
             "/contact 184.123.665.067\n",
             "/home 184.123.665.067\n",
@@ -17,7 +18,7 @@ RSpec.describe Counter::Total do
         "/about/2" => { :count => 1, :ips => [] },
         "/contact" => { :count => 1, :ips => [] },
         "/help_page/1" => { :count => 1, :ips => [] },
-        "/home" => { :count => 2, :ips => [] }
+        "/home" => { :count => 3, :ips => [] }
     }
 
     it { is_expected.to eq(hash) }
